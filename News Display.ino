e03b26274bd333808d7e71d70964c5ba8a919131
b45i <amalshajan2011@gmail.com>
2021-06-17 19:00:35 +0530

    dynamic image

--- News Display.ino
+++ News Display.ino
@@ -60,6 +60,64 @@ bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap) 
   return 1;
 }
 
+String urlEncode(String str)
+{
+    String encodedString="";
+    char c;
+    char code0;
+    char code1;
+    char code2;
+    for (int i =0; i < str.length(); i++){
+      c=str.charAt(i);
+      if (c == ' '){
+        encodedString+= '+';
+      } else if (isalnum(c)){
+        encodedString+=c;
+      } else{
+        code1=(c & 0xf)+'0';
+        if ((c & 0xf) >9){
+            code1=(c & 0xf) - 10 + 'A';
+        }
+        c=(c>>4)&0xf;
+        code0=c+'0';
+        if (c > 9){
+            code0=c - 10 + 'A';
+        }
+        code2='\0';
+        encodedString+='%';
+        encodedString+=code0;
+        encodedString+=code1;
+      }
+      yield();
+    }
+    return encodedString;
+    
+}
+
+void downloadImage(String title, String imgURL, String  date, String author) {
+    title = "title=" + urlEncode(title);
+    imgURL = "&imgURL=" + imgURL;
+    date = "&date=" + urlEncode(date);
+    author = "&author=" + urlEncode(author);
+    String url = "http://news-image-api.herokuapp.com/?" + title + imgURL + date + author;
+    Serial.println(url);
+    File f = SPIFFS.open("/img.jpg", "w");
+    if (f) {
+      http.begin(url);
+      int httpCode = http.GET();
+      if (httpCode > 0) {
+        if (httpCode == HTTP_CODE_OK) {
+          http.writeToStream(&f);
+        }
+      } else {
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+      f.close();
+    }
+    http.end();
+    loadFile();
+}
+
 
 void loadFile() {
   tft.fillScreen(TFT_BLACK);
@@ -79,25 +137,6 @@ void setup() {
   setupWiFi();
 }
 
-void downloadImage() {
-    String url = "http://news-image-api.herokuapp.com/";
-    Serial.println(url);
-    File f = SPIFFS.open("/img.jpg", "w");
-    if (f) {
-      http.begin(url);
-      int httpCode = http.GET();
-      if (httpCode > 0) {
-        if (httpCode == HTTP_CODE_OK) {
-          http.writeToStream(&f);
-        }
-      } else {
-        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-      }
-      f.close();
-    }
-    http.end();
-}
-
 void loop() {
   // fs::Dir directory = SPIFFS.openDir("/");
   // while (directory.next()) {
@@ -107,7 +146,11 @@ void loop() {
   //   }
   // }
   // const char *name = '/.img.jpg';
-  downloadImage();
-  loadFile();
+  downloadImage(
+    "NEO Surveyor: Space telescope to detect potentially dangerous asteroids heading for Earth - WION",
+    "https://cdn.wionews.com/sites/default/files/styles/story_page/public/2021/06/17/198836-neo-surveyor.jpg",
+    "2021-06-17",
+    "WION Web Team"
+  );
   delay(5000);
 }
