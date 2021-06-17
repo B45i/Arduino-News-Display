153db3d1e15666eb1e687ba62333e0ae3b868c7b
b45i <amalshajan2011@gmail.com>
2021-06-17 18:11:42 +0530

    image from web

--- News Display.ino
+++ News Display.ino
@@ -1,3 +1,5 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 #include <TJpg_Decoder.h>
 #define FS_NO_GLOBALS
 #include <FS.h>
@@ -6,6 +8,10 @@
 
 
 TFT_eSPI tft = TFT_eSPI();
+HTTPClient http;
+
+const char* SSID = "ZTE_2.4G_ExQCMa";                 // Your WiFi SSID
+const char* PASSWORD = "NullReferenceException#123";  // Your WiFi Password
 
 // Display SDO/MISO to NodeMCU pin D6 (or leave disconnected if not reading TFT)
 // Display LED to NodeMCU pin VIN (or 5V, see below)
@@ -17,52 +23,82 @@ TFT_eSPI tft = TFT_eSPI();
 // Display GND to NodeMCU pin GND (0V)
 // Display VCC to NodeMCU 5V or 3.3V
 
-bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap)
-{
+
+void setupTFT() {
+  tft.begin();
+  tft.setTextColor(0xFFFF, 0x0000);
+  tft.fillScreen(TFT_BLACK);
+  tft.setRotation(3);
+
+  TJpgDec.setJpgScale(1);
+  TJpgDec.setSwapBytes(true);
+  TJpgDec.setCallback(tft_output);
+}
+
+void setupWiFi() {
+  tft.setTextColor(ILI9341_YELLOW);
+  tft.setTextSize(2);
+  Serial.print("\nConnecting...");
+  tft.print("\nConnecting...");
+
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    tft.print(".");
+    delay(500);
+  }
+  Serial.print("\nConnected - ");
+  Serial.println(WiFi.localIP());
+  tft.print("\nConnected - ");
+  tft.println(WiFi.localIP());
+}
+
+
+bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap) {
   if ( y >= tft.height() ) return 0;
   tft.pushImage(x, y, w, h, bitmap);
   return 1;
 }
 
 
-void loadFile()
-{
+void loadFile() {
   tft.fillScreen(TFT_BLACK);
   TJpgDec.drawFsJpg(0, 0, "/img.jpg");
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
-  Serial.println("\n\n Testing TJpg_Decoder library");
 
-  // Initialise SPIFFS
   if (!SPIFFS.begin()) {
     Serial.println("SPIFFS initialisation failed!");
     while (1) yield();
   }
-  Serial.println("\r\nInitialisation done.");
+  Serial.println("SPIFFS Initialisation done.");
 
-  // Initialise the TFT
-  tft.begin();
-  tft.setTextColor(0xFFFF, 0x0000);
-  tft.fillScreen(TFT_BLACK);
-  tft.setRotation(3); 
-
-
-  // The jpeg image can be scaled by a factor of 1, 2, 4, or 8
-  TJpgDec.setJpgScale(1);
-
-  // The byte order can be swapped (set true for TFT_eSPI)
-  TJpgDec.setSwapBytes(true);
-
-  // The decoder must be given the exact name of the rendering function above
-  TJpgDec.setCallback(tft_output);
+  setupTFT();
+  setupWiFi();
 }
 
+void downloadImage() {
+    String url = "http://news-image-api.herokuapp.com/";
+    Serial.println(url);
+    File f = SPIFFS.open("/img.jpg", "w");
+    if (f) {
+      http.begin(url);
+      int httpCode = http.GET();
+      if (httpCode > 0) {
+        if (httpCode == HTTP_CODE_OK) {
+          http.writeToStream(&f);
+        }
+      } else {
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+      f.close();
+    }
+    http.end();
+}
 
-void loop()
-{
+void loop() {
   // fs::Dir directory = SPIFFS.openDir("/");
   // while (directory.next()) {
   //   String strname = directory.fileName();
@@ -71,6 +107,7 @@ void loop()
   //   }
   // }
   // const char *name = '/.img.jpg';
+  downloadImage();
   loadFile();
   delay(5000);
 }
